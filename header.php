15e5d5f383a5bc8d88506b378c2d74c5e4942f72
Redcube4983 <it@redcube.jp>
2024-07-12 17:05:39 +0900

    waka

---
+++ header.php
@@ -0,0 +1,162 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<meta content="IE=edge" http-equiv="X-UA-Compatible">
+<meta content="width=device-width,initial-scale=1" name="viewport">
+<title>ATAC</title>
+<meta name="format-detection" content="telephone=no,address=no,email=no">
+
+<!--404ページ-->
+<!--<meta http-equiv="refresh" content="10 ; URL=/">-->
+
+<!--ファビコン-->
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/common/images/logo.svg">
+
+<!--スタイルシート-->
+<link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
+
+<!--front-page-->
+<?php if( is_front_page() ): ?>
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/front-page.css">
+<?php else: ?>
+<?php endif; ?>
+
+<?php
+    // slug名のcssがあれば読み込む
+    global $post;
+    $slugName = $post->post_name;
+    if (isset($slugName)) :
+      if (is_file(get_theme_file_path('css/' . $slugName . '.css')) && file_exists(get_theme_file_path('css/' . $slugName . '.css'))) :
+    ?>
+        <link rel="stylesheet" href="<?php echo get_theme_file_uri('css/' . $slugName . '.css'); ?>" />
+  <?php
+    endif;
+  endif;
+?>
+
+
+<!--jquery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<!--fontawesome-->
+<!--<script src="https://kit.fontawesome.com/f01c716632.js"></script>-->
+
+<!--googlefont-->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Zen+Maru+Gothic&display=swap" rel="stylesheet">
+
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-xxxxxxxxxx-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-xxxxxxxxxx-1');
+</script>
+<?php wp_head(); ?>
+</head>
+
+<body>
+<div class="wrapper">
+<header class="l-header">
+<h1 class="l-header__title">
+  <a href="/" class="l-header__link">
+    <img class="l-header__sitelogo" src="<?php echo get_template_directory_uri(); ?>/common/images/logo.svg" alt="総合保険代理店 ATAC">
+  </a>
+</h1>
+<!--下層ページ
+<div class="l-header__title">
+  <a href="/" class="l-header__title__link">
+    <img class="l-header__title__sitelogo" src="common/images/kogyokusha_logo.svg" alt="攻玉社 中学校・高等学校">
+  </a>
+</div>
+-->
+<div class="l-header__right">
+<nav class="l-subnav">
+  <div class="l-subnav__inner">
+    <p>ご相談はこちら</p>
+      <ul class="l-subnav__list">
+        <li>
+          <a class="l-subnav__link" href="#">
+            <picture>
+              <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_tel.webp" type="image/webp">
+              <img src="<?php echo get_template_directory_uri(); ?>/common/images/icon_tel.png" alt="電話">
+            </picture>
+          </a>
+        </li>
+        <li
+          <a class="l-subnav__link" href="#">
+            <picture>
+              <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_mail.webp" type="image/webp">
+              <img src="<?php echo get_template_directory_uri(); ?>/common/images/icon_mail.png" alt="メール">
+            </picture>
+          </a>
+        </li>
+      </ul>
+    </div>
+</nav>
+<div class="l-header__toggle js-navSwitch">
+  <div class="l-header__inner">
+    <span class="l-header__inner--bar1"></span>
+    <span class="l-header__inner--bar2"></span>
+    <span class="l-header__inner--bar3"></span>
+  </div>
+  <p class="l-header__text js-navText">MENU</p>
+</div>
+<nav class="l-globalnav js-globalNav">
+    <ul class="l-globalnav__inner">
+      <li class="l-globalnav__list l-globalnav__list--parent">
+        <div class="l-globalnav__block">
+          <a class="l-globalnav__link" href="#">
+            <picture>
+              <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_ATAC.webp" type="image/webp">
+              <img class="icon_img" src="<?php echo get_template_directory_uri(); ?>/common/images/icon_ATAC.png" alt="">
+            </picture>
+            <span>当社のご案内</span>
+          </a>
+          <span class="js-globalnavlinkArrow l-globalnav__link--arrow"></span>
+        </div>
+        <ul class="l-globalnav__child">
+          <li class="l-globalnav__granchild">
+            <a class="l-globalnav__link" href="#">BLOG</a>
+          </li>
+        </ul>
+      </li>
+      <li class="l-globalnav__list l-globalnav__list--single last">
+        <a href="#">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_insurance.webp" type="image/webp">
+            <img class="icon_img" src="<?php echo get_template_directory_uri(); ?>/common/images/icon_insurance.png" alt="">
+          </picture>
+          <span>保険のご案内</span>
+        </a>
+      </li>
+      <li class="l-globalnav__list l-globalnav__list--single last">
+        <a href="#">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_Q.webp" type="image/webp">
+            <img class="icon_img" src="<?php echo get_template_directory_uri(); ?>/common/images/icon_Q.png" alt="">
+          </picture>
+          <span>よくあるご質問</span>
+        </a>
+      </li>
+      <li class="l-globalnav__list l-globalnav__list--single last">
+        <a href="#">
+          <picture>
+            <source srcset="<?php echo get_template_directory_uri(); ?>/common/images/icon_info.webp" type="image/webp">
+            <img class="icon_img" src="<?php echo get_template_directory_uri(); ?>/common/images/icon_info.png" alt="">
+          </picture>
+          <span>お役立ち情報</span>
+        </a>
+      </li>
+      <li class="l-globalnav__list l-globalnav__list--parent l-globalnav__list--pc">
+        <div class="l-globalnav__block"><a class="l-globalnav__link l-globalnav__contact__link" href="#">ご相談はこちら</a><span class="js-globalnavlinkArrow l-globalnav__link--arrow"></span></div>
+      </li>
+    </ul>
+</nav>
+</div>
+</header>
---
+++ page-about-us.php
@@ -0,0 +1,188 @@
+<?php
+/*
+Template Name: about-us
+*/
+?>
+<?php get_header(); ?>
+<main>
+  <section id="about-top">
+    <div id="about-top_area">
+        <img class="abut_image01" src="images/about-page/about-window.png" alt="画像1">
+        <img class="abut_title" src="images/about-page/title.png" alt="当社のご案内">
+        <img class="abut_image02" src="images/about-page/about-sofa.png" alt="画像2">
+    </div>
+  </section>
+
+
+
+
+  <section id="p-aboutUsContents">
+    <div class="p-aboutUsContents__infoArea">
+      <div class="p-aboutUsContents__company">
+        <div class="p-aboutUsContents__infoArea--title">
+          <h2>会社概要</h2>
+        </div>
+        <div class="p-aboutUsContents__infoArea--box1">
+          <dl class="p-aboutUsContents__item">
+            <dt class="p-profile__list__head">社名</dt>
+            <dd class="p-profile__list__body">株式会社ATAC</dd>
+            <dt class="p-profile__list__head">住所</dt>
+            <dd class="p-profile__list__body">〒292-0014千葉県木更津市高柳1-8-1</dd>
+            <dt class="p-profile__list__head">TEL</dt>
+            <dd class="p-profile__list__body">0438-41-6011</dd>
+            <dt class="p-profile__list__head">FAX</dt>
+            <dd class="p-profile__list__body">0438-41-6006</dd>
+            <dt class="p-profile__list__head">E-mail</dt>
+            <dd class="p-profile__list__body">info.6011@atac.jp</dd>
+            <dt class="p-profile__list__head">定休日</dt>
+            <dd class="p-profile__list__body">日曜・祝日（ご予約頂いた場合はＯＫです。）</dd>
+            <dt class="p-profile__list__head">代表者</dt>
+            <dd class="p-profile__list__body">青木　亮</dd>
+            <dt class="p-profile__list__head">事業内容</dt>
+            <dd class="p-profile__list__body">
+              <ol class="p-numberlist">
+                <li class="p-numberlist__item">
+                  各種保険代理業（各種生命保険・各種損害保険）
+                </li>
+                <li class="p-numberlist__item">
+                  各種セミナー講師派遣（事業継承問題・相続問題・資産運用など）
+                </li>
+                <li class="p-numberlist__item">
+                  ファイナンシャルプランナーによる個別相談会
+                </li>
+              </ol>
+            </dd>
+          </dl>
+        </div>
+        <div class="p-aboutUsContents__infoArea--box2">
+          <a href="">
+            <b>取扱保険会社</b>
+            <ul class="l-footer__top--companyLogo">
+              <li>
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/aioiseimei.webp" type="image/webp">
+                  <img src="<?php echo get_template_directory_uri(); ?>/images/common/aioiseimei.png" alt="">
+                </picture>
+              </li>
+              <li>
+                <picture>
+                  <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/sumitomokaijyo.webp" type="image/webp">
+                  <img src="<?php echo get_template_directory_uri(); ?>/images/common/sumitomokaijyo.png" alt="">
+                </picture>
+              </li>
+            </ul>
+          </a>
+        </div>
+      </div>
+
+      <div class="p-aboutUsContents__policy">
+        <div class="p-aboutUsContents__infoArea--title">
+          <h2>経営方針</h2>
+        </div>
+          <ul class="p-aboutUsContents__infoArea--box1 p-aboutUsContents__policy--list">
+            <li>
+              <span>01</span>
+              <p>保険業務知識はもとよりその周辺知識を貪欲に吸収し、真のプロを目指す</p>
+            </li>
+            <li>
+              <span>02</span>
+              <p>保険／金融のプロとしてお客さまの願いや価値観をしっかり聞き、お客さまが描く未来予想図の実現のお手伝いをする</p>
+            </li>
+            <li>
+              <span>03</span>
+              <p>常に、コンプライアンス遵守を最優先とし、お客さまの信頼に応えていく</p>
+            </li>
+            <li>
+              <span>04</span>
+              <p>自由に意見の云える明るいアットホームな職場環境を整備していく</p>
+            </li>
+            <li>
+              <span>05</span>
+              <p>地域社会から必要とされるライフプランナーを目指す</p>
+            </li>
+          </ul>
+      </div>
+
+      <div class="p-aboutUsContents__philosophy">
+        <div class="p-aboutUsContents__infoArea--title">
+          <h2>経営理念</h2>
+        </div>
+        <div class="p-aboutUsContents__infoArea--box1 p-aboutUsContents__inner">
+          <div class="p-aboutUsContents__inner--text1">
+            <p>
+            時には、良きアドバイザーとして… 時には、良きコンシェルジュとして… 
+            お客様と信頼で結ばれた『人生のパートナー』であり続けたい。
+            ATACは、ライフプランニングから始まる、お客様への生涯サポートを安心とともにお届けします。
+            </p>
+          </div>
+          <div class="p-aboutUsContents__inner--text2">
+            <p class="p-aboutUsContents__inner--weightBold">保険は目に見えない“将来の保証”を買うもの。</p>
+            <p>だから良い買い物をしたのかどうかは、いざその時になってみないとわかりません。 
+              保険を選ぶ時点で、重要となるポイントは『なぜこの保険設計になったのか？』ということを、 
+              ご自身が十分納得できているかどうかではないでしょうか？
+            </p>
+          </div>
+        </div>
+        <div class="p-aboutUsContents__comment">
+          <p class="p-aboutUsContents__comment--title">我々にお任せ下さい！！</p>
+          <p>ATACは、保険業務知識はもとより周辺知識を貧欲に吸収し、保険・金融のプロとして
+              お客様の経済的変化を把握し、一人ひとりに適したアドバイスをさせていただきます。 
+              万一の際の迅速な対応はもちろん、ご家族のゆく末までしっかりと見守り続けます。
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
+  <div id="l-management-content">
+    <div class="management-content-innner">
+      <img class="policy" src="images/about-page/about-fusen.png">
+      <h2 class="p-page__lead">経営方針</h2>
+
+      <div class="p-management__item">
+        <div class="p-management__inner">
+          <div class="p-management__number">
+            <div class="p-management__num--round">
+              <span class="num">01</span>
+            </div>
+            
+          </div>
+          <div class="p-management__number">
+            <div class="p-management__num--round">
+              <span class="num">02</span>
+            </div>
+            <p class="p-management__lead">
+              
+            </p>
+          </div>
+          <div class="p-management__number">
+            <div class="p-management__num--round">
+              <span class="num">03</span>
+            </div>
+            <p class="p-management__lead">
+              
+            </p>
+          </div>
+          <div class="p-management__number">
+            <div class="p-management__num--round">
+              <span class="num">04</span>
+            </div>
+            <p class="p-management__lead">
+              
+            </p>
+          </div>
+          <div class="p-management__number">
+            <div class="p-management__num--round">
+              <span class="num">05</span>
+            </div>
+            <p class="p-management__lead">
+              地域社会から必要とされるライフプランナーを目指す
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+<?php get_footer(); ?>
