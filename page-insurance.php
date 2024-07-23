99bb241183b7613c6a375c83e20e91a91761b4dd
Redcube4983 <it@redcube.jp>
2024-07-23 19:06:43 +0900

    waka

--- page-insurance.php
+++ page-insurance.php
@@ -6,12 +6,20 @@ Template Name: insurance
 <?php get_header(); ?>
 <main>
   <section id="l-internalTop">
+
+
     <div class="l-internalTop__ornamentLeft">
       <picture>
         <source srcset="<?php echo get_template_directory_uri(); ?>/images/insurance/ornament.webp" type="image/webp">
         <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/ornament.png" alt="">
       </picture>
     </div>
+    <!-- <div class="l-internalTop__ornamentRight">
+      <picture>
+        <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/illust.webp" type="image/webp">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/common/illust.png" alt="">
+      </picture>
+    </div> -->
     <h1 class="l-internalTop__title">
       <div class="l-internalTop__text">
         <picture>
@@ -25,12 +33,6 @@ Template Name: insurance
           <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/ornamentLine.png" alt="">
         </picture>
       </div>
-      <div class="l-internalTop__illust">
-        <picture>
-          <source srcset="<?php echo get_template_directory_uri(); ?>/images/common/illust.webp" type="image/webp">
-          <img src="<?php echo get_template_directory_uri(); ?>/images/common/illust.png" alt="">
-        </picture>
-      </div>
     </h1>
   </section>
 
@@ -40,21 +42,23 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>生命保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>万一の時、将来のために</b>
-            </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
-                <li>&#9632;&amp;LIFE 新総合収入保障</li>
-                <li>&#9632;&amp;LIFE 医療保険Ａエースセレクト</li>
-                <li>&#9632;&amp;LIFE 逓減定期保険</li>
-                <li>&#9632;&amp;LIFE こども保険</li>
-                <li>&#9632;&amp;LIFE 終身保険（低解約返戻金型）</li>
-                <li>&#9632;&amp;LIFE 新収入保障</li>
-                <li>&#9632;&amp;LIFE ガン保険Ｓスマートセレクト</li>
-                <li>&#9632;&amp;LIFE 個人年金保険</li>
-                <li>&#9632;&amp;特定疾病保障終身保険</li>
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+          <b>万一の時、将来のために</b>
+          <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+            <ul class="">
+              <li>&#9632;&amp;LIFE 新総合収入保障</li>
+              <li>&#9632;&amp;LIFE 医療保険Ａエースセレクト</li>
+              <li>&#9632;&amp;LIFE 逓減定期保険</li>
+              <li>&#9632;&amp;LIFE こども保険</li>
+              <li>&#9632;&amp;LIFE 終身保険（低解約返戻金型）</li>
             </ul>
+            <ul class="">
+              <li>&#9632;&amp;LIFE 新収入保障</li>
+              <li>&#9632;&amp;LIFE ガン保険Ｓスマートセレクト</li>
+              <li>&#9632;&amp;LIFE 個人年金保険</li>
+              <li>&#9632;&amp;特定疾病保障終身保険</li>
+            </ul>
+          </div>
         </div>
       </div>
 
@@ -62,39 +66,42 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>自動車保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>カーライフに安心を</b>
-            </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
-                <li>&#9632;GK クルマの保険（家庭用自動車総合保険）</li>
-                <li>&#9632;はじめての自動車保険（個人用自動車保険）</li>
-                <li>&#9632;自賠責保険</li>
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+          <b>カーライフに安心を</b>
+          <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+            <ul class="">
+              <li>&#9632;GK クルマの保険（家庭用自動車総合保険）</li>
+              <li>&#9632;はじめての自動車保険（個人用自動車保険）</li>
+              <li>&#9632;自賠責保険</li>
             </ul>
-            <div class="p-aboutUsContents__carImg">
+            <ul class="">
+              <li>
                 <a href="https://www.btoc.ms-ins.com/qsc/QSC00000">
-                    <picture>
-                        <source srcset="<?php echo get_template_directory_uri(); ?>/images/insurance/car.webp" type="image/webp">
-                        <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/car.png" alt="">
-                    </picture>
+                  <picture>
+                      <source srcset="<?php echo get_template_directory_uri(); ?>/images/insurance/car.webp" type="image/webp">
+                      <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/car.png" alt="">
+                  </picture>
                 </a>
-            </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
+
 
       <div class="p-aboutUsContents__insuranceBox">
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>火災保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>快適で安心な暮らしのために</b>
-            </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+            <b>快適で安心な暮らしのために</b>
+            <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+              <ul class="">
                 <li>&#9632;GK すまいの保険（家庭用火災保険）</li>
                 <li>&#9632;リビングＦＩＴ（賃貸住宅居住者総合保険）</li>
                 <li>&#9632;地震保険</li>
-            </ul>
+              </ul>
+            </div>
         </div>
       </div>
 
@@ -102,14 +109,14 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>ケガの保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>突然の事故によるケガに</b>
-            </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
-                <li>&#9632;GKケガの保険（パーソナル総合傷害保険）</li>
-                <li>&#9632;晴れやか世代（特定傷害保険）</li>
-            </ul>
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+              <b>突然の事故によるケガに</b>
+              <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+                <ul class="">
+                    <li>&#9632;GKケガの保険（パーソナル総合傷害保険）</li>
+                    <li>&#9632;晴れやか世代（特定傷害保険）</li>
+                </ul>
+              </div>
         </div>
       </div>
 
@@ -117,14 +124,24 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>旅行の保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>旅先での突然のトラブルに</b>
-            </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
-                <li>&#9632;海外旅行保険</li>
-                <li>&#9632;国内旅行傷害保険</li>
-            </ul>
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+              <b>旅先での突然のトラブルに</b>
+              <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+                <ul class="">
+                    <li>&#9632;海外旅行保険</li>
+                    <li>&#9632;国内旅行傷害保険</li>
+                </ul>
+                <ul class="">
+                  <li>
+                    <a href="https://www.net.ms-ins.com/servlet/NAB_UII_UG0010">
+                      <picture>
+                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/insurance/abroad.webp" type="image/webp">
+                          <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/abroad.png" alt="">
+                      </picture>
+                    </a>
+                  </li>
+                </ul>
+              </div>
         </div>
       </div>
 
@@ -132,14 +149,24 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>レジャーの保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
-            <div class="p-aboutUsContents__insuranceBox-lead">
-                <b>レジャーを安心して楽しむために</b>
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
+            <b>レジャーを安心して楽しむために</b>
+            <div class="p-aboutUsContents__insuranceBox-whiteArea-inner">
+              <ul class="">
+                  <li>&#9632;ゴルファー保険</li>
+                  <li>&#9632;レクレーション傷害補償プラン</li>
+              </ul>
+              <ul class="">
+                  <li>
+                    <a href="https://net2.ms-ins.com/servlet/sgl/SGL_UII_GLS0010">
+                      <picture>
+                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/insurance/golf.webp" type="image/webp">
+                          <img src="<?php echo get_template_directory_uri(); ?>/images/insurance/golf.png" alt="">
+                      </picture>
+                    </a>
+                  </li>
+                </ul>
             </div>
-            <ul class="p-aboutUsContents__insuranceBox-kinds">
-                <li>&#9632;ゴルファー保険</li>
-                <li>&#9632;レクレーション傷害補償プラン</li>
-            </ul>
         </div>
       </div>
 
@@ -147,7 +174,7 @@ Template Name: insurance
         <div class="p-aboutUsContents__insuranceBox-title">
             <h2>その他の保険</h2>
         </div>
-        <div class="p-aboutUsContents__insuranceBox-textbox">
+        <div class="p-aboutUsContents__insuranceBox-whiteArea">
             <div class="p-aboutUsContents__insuranceBox-products">
                 <a href="https://www.ms-ins.com/business/">
                 <span>企業向け商品・サービス</span>
