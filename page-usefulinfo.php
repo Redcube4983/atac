54c8529bf283a56b750c37b177e840a07f3ed1aa
Redcube4983 <it@redcube.jp>
2024-07-30 19:09:07 +0900

    waka

--- page-usefulinfo.php
+++ page-usefulinfo.php
@@ -37,155 +37,121 @@ Template Name: usefulinfo
   <section id="p-usefulinfoContents">
     <div class="p-usefulinfoContents__usefulinfoArea">
       <div class="c-contentsLayout">
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>安全運転や健康、保険に関する情報集です！</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-            <b>事故多発交差点</b>
-            <p>事故が多発している交差点を「曜日別」「時間帯別」など１１項目で分析しているコーナーです。お近くを通る際の情報としてご参考ください。</p>
-            <div class="c-usefulinfoContents__usefulinfoBox--btn">
-                <span>詳しくはこちらをチェック！</span>
-            </div>
+        <div class="p-usefulinfoContents__usefulinfoBox">
+          <div class="p-usefulinfoContents__usefulinfoBox-title">
+              <h2>安全運転や健康、保険に関する情報集です！</h2>
           </div>
-        </div>
-      </div>
-
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>自動車保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-          <b>カーライフに安心を</b>
-          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-            <ul>
-              <li>&#9632;GK クルマの保険（家庭用自動車総合保険）</li>
-              <li>&#9632;はじめての自動車保険（個人用自動車保険）</li>
-              <li>&#9632;自賠責保険</li>
-            </ul>
-            <ul class="">
-              <li>
-                <a href="https://www.btoc.ms-ins.com/qsc/QSC00000" rel="noopener noreferrer" target="_blank">
-                  <picture>
-                      <source srcset="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/car.webp" type="image/webp">
-                      <img src="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/car.png" alt="">
-                  </picture>
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>事故多発交差点</b>
+              <p>事故が多発している交差点を「曜日別」「時間帯別」など１１項目で分析しているコーナーです。お近くを通る際の情報としてご参考ください。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.sonpo.or.jp/protection/kousaten/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
                 </a>
-              </li>
-            </ul>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
 
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>火災保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-            <b>快適で安心な暮らしのために</b>
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
             <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-              <ul class="">
-                <li>&#9632;GK すまいの保険（家庭用火災保険）</li>
-                <li>&#9632;リビングＦＩＴ（賃貸住宅居住者総合保険）</li>
-                <li>&#9632;地震保険</li>
-              </ul>
-            </div>
-        </div>
-      </div>
-
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>ケガの保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-              <b>突然の事故によるケガに</b>
-              <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-                <ul class="">
-                    <li>&#9632;GKケガの保険（パーソナル総合傷害保険）</li>
-                    <li>&#9632;晴れやか世代（特定傷害保険）</li>
-                </ul>
+              <b>安全運転のポイント</b>
+              <p>安全運転や事故防止のポイントを時事テーマをもとにまとめています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.dairitenhp.com/anzen/anzen_top.htm" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
               </div>
-        </div>
-      </div>
+            </div>
+          </div>
 
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>旅行の保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-              <b>旅先での突然のトラブルに</b>
-              <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-                <ul class="">
-                    <li>&#9632;海外旅行保険</li>
-                    <li>&#9632;国内旅行傷害保険</li>
-                </ul>
-                <ul class="">
-                  <li>
-                    <a href="https://www.net.ms-ins.com/servlet/NAB_UII_UG0010" rel="noopener noreferrer" target="_blank">
-                      <picture>
-                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/abroad.webp" type="image/webp">
-                          <img src="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/abroad.png" alt="">
-                      </picture>
-                    </a>
-                  </li>
-                </ul>
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>日本損害保険協会</b>
+              <p>自動車の事故や災害に遭ったときに役立つ情報、防災情報および各種統計等を提供しています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.sonpo.or.jp/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
               </div>
-        </div>
-      </div>
-
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>レジャーの保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
-            <b>レジャーを安心して楽しむために</b>
-            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-              <ul class="">
-                  <li>&#9632;ゴルファー保険</li>
-                  <li>&#9632;レクレーション傷害補償プラン</li>
-              </ul>
-              <ul>
-                  <li>
-                    <a href="https://net2.ms-ins.com/servlet/sgl/SGL_UII_GLS0010" rel="noopener noreferrer" target="_blank">
-                      <picture>
-                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/golf.webp" type="image/webp">
-                          <img src="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/golf.png" alt="">
-                      </picture>
-                    </a>
-                  </li>
-                </ul>
             </div>
-        </div>
-      </div>
+          </div>
 
-      <div class="p-usefulinfoContents__usefulinfoBox">
-        <div class="p-usefulinfoContents__usefulinfoBox-title">
-            <h2>その他の保険</h2>
-        </div>
-        <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
             <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
-              <ul>
-                  <li>
-                    <a href="https://www.ms-ins.com/business/" rel="noopener noreferrer" target="_blank">
-                      <picture>
-                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/service.webp" type="image/webp">
-                          <img src="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/service.png" alt="">
-                      </picture>
-                    </a>
-                  </li>
-              </ul>
-              <ul>
-                  <li>
-                    <a href="https://net2.ms-ins.com/servlet/sgl/SGL_UII_GLS0010" rel="noopener noreferrer" target="_blank">
-                      <picture>
-                          <source srcset="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/bicycle.webp" type="image/webp">
-                          <img src="<?php echo get_template_directory_uri(); ?>/images/usefulinfo/bicycle.png" alt="">
-                      </picture>
-                    </a>
-                  </li>
-                </ul>
+              <b>生命保険協会</b>
+              <p>生命保険の基礎知識、生命保険協会からの最新のニュース等を提供しています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.seiho.or.jp/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
             </div>
+          </div>
+
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>厚生労働省-先進医療</b>
+              <p>厚生労働省のホームページから先進医療についての情報を提供しています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.mhlw.go.jp/topics/bukyoku/isei/sensiniryo/index.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>生命保険文化センター</b>
+              <p>生命保険に関する全国実態調査についての情報提供しています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.jili.or.jp/research/report/zenkokujittai.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>ゴールドリボン・ネットワーク</b>
+              <p>ゴールドリボン・ネットワークは、小児がんの治療法や薬、経験者の生活の質（QOL）向上のための
+              研究開発を支援する非営利団体です。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.goldribbon.jp/index.htm" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>J.POSH 日本乳がんピンクリボン運動</b>
+              <p>私たちのスローガンである”for the future generation”明日を担う次の世代のために、
+              明日の健康な社会を残せるよう活動しています。</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.j-posh.com/index.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-usefulinfoContents__usefulinfoBox-whiteArea">
+            <div class="p-usefulinfoContents__usefulinfoBox-whiteArea-inner">
+              <b>日本脳卒中協会</b>
+              <p>脳卒中に関する正しい知識の普及および社会啓発によって新たな脳卒中発症の減少に寄与すると同時に、
+                脳卒中患者・家族に対する情報提供、自立のための支援を促進することによって、患者・家族の不安と
+                ハンディキャップの軽減と生活の質の改善を図り、その成果を通じて国民の保健、福祉の向上に寄与して行く事が目的…</p>
+              <div class="c-usefulinfoContents__usefulinfoBox--btn">
+                <a href="http://www.jsa-web.org/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
