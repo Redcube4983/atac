e1d892a8f14bcbdc247b2740a750079d049de13b
Redcube4983 <it@redcube.jp>
2024-08-01 18:49:05 +0900

    waka

---
+++ page-privacy-policy.php
@@ -0,0 +1,280 @@
+<?php
+/*
+Template Name: privacy-policy
+*/
+?>
+<?php get_header(); ?>
+<main>
+  <section id="l-internalTop">
+    <h1 class="l-internalTop__title">
+      <div class="l-internalTop__text">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/pageTitle.webp" type="image/webp">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/pageTitle.png" alt="プライバシーポリシー">
+        </picture>
+      </div>
+      <div class="l-internalTop__underLine">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/ornamentLine.webp" type="image/webp">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/ornamentLine.png" alt="">
+        </picture>
+      </div>
+    </h1>
+  </section>
+
+  <section id="p-privacyContents">
+    <div class="p-privacyContents__privacyArea">
+      <div class="c-contentsLayout">
+        <div class="p-privacyContents__privacyBox">
+          <div class="p-privacyContents__privacyBox-title">
+              <h2>個人情報保護法宣言</h2>
+          </div>
+          <div class="p-privacyContents__privacyBox-inner">
+            <p>
+            当代理店は、個人情報保護の重要性に鑑み、また、保険業に対する社会の信頼をより向上させるため、個人情報の保護に関する法律（個人情報保護法）、行政手続における特定の個人を識別するための番号の利用等に関する法律（番号法）その他の関連法令、関係官庁からのガイドライン、特定個人情報の適正な取り扱いに関するガイドライン等を遵守して、個人情報を適正に取り扱うとともに、安全管理について適切な措置を講じます。 当代理店は、個人情報の取り扱いが適正に行われるよう、従業者への教育・指導を徹底し、適正な取り扱いに取り組んでまいります。
+            また、個人情報の取り扱いに関する苦情・相談に迅速に対応し、当代理店の個人情報の取り扱いおよび安全管理に係る適切な措置については、適宜見直し、改善いたします。
+            </p>
+          </div>
+        </div>
+
+        <div class="p-privacyContents__numberList">
+            <div class="p-privacyContents__numberList-title">
+                <h3>1.個人情報の取得・利用</h3>
+            </div>
+            <div class="p-privacyContents__numberList-inner">
+                <p>
+                当代理店は、業務上必要な範囲内で、かつ、適法で公正な手段により個人情報を取得・利用します。
+                （下記６．の個人番号および特定個人情報を除きます。）
+                </p>
+            </div>
+        </div>
+        <div class="p-privacyContents__numberList">
+            <div class="p-privacyContents__numberList-title">
+                <h3>2.個人情報の利用目的</h3>
+            </div>
+            <div class="p-privacyContents__numberList-inner">
+                <p>
+                当代理店は、下記の各保険会社から保険業務の委託を受けた代理店であり、取得した個人情報（個人番号および特定個人情報については、下記６．をご覧ください。）を、各社の商品・サービスのご提供のために必要な範囲で利用します。
+                上記の利用目的を変更する場合には、その内容をご本人に対し、原則として書面（電磁的記録を含む。以下同じ。）等により
+                ご通知し、またはホームページへの掲載などの方法により公表します。
+                各保険会社の個人情報の利用目的は、各社のホームページに記載してあります。
+                </p>
+                <div class="p-privacyContents__numberList-inner-insurance">
+                    <span>&#60;損害保険会社&#62;</span>
+                    <a href="http://www.ms-ins.com" rel="noopener noreferrer" target="_blank">
+                        <p>&#127019;三井住友海上火災保険株式会社（http://www.ms-ins.com）
+                        </p>
+                    </a>
+                </div>
+                <div class="p-privacyContents__numberList-inner-insurance">
+                    <span>&#60;生命保険会社&#62;</span>
+                    <a href="http://www.msa-life.co.jp" rel="noopener noreferrer" target="_blank">
+                        <p>&#127019;三井住友海上あいおい生命保険株式会社（http://www.msa-life.co.jp）
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="p-privacyContents__numberList">
+            <div class="p-privacyContents__numberList-title">
+                <h3>3.個人データの安全管理措置</h3>
+            </div>
+            <div class="p-privacyContents__numberList-inner">
+                <p>
+                当代理店は、取り扱う個人データ（下記６．の個人番号および特定個人情報を含みます。）の漏えい、滅失またはき損の防止
+                その他の個人データの安全管理のため、安全管理に関する取扱規程等の整備および実施体制の整備等、十分なセキュリティ対策を講じるとともに、利用目的の達成に必要とされる正確性・最新性を確保するために適切な措置を講じています。
+                当社は、個人データの安全管理措置に関する社内規程を別途定めており、その具体的内容は主として以下のとおりです。
+                安全管理措置に関するご質問については、下記８．のお問い合わせ窓口までお寄せください。
+                </p>
+            </div>
+            <div class="p-privacyContents__numberList3-num">
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（1）基本方針の整備</h3>
+                </div>
+                <p>個人データの適正な取扱いの確保のため、「関係法令・ガイドライン等の遵守」、「安全管理措置に関する事項」、「質問および苦情処理の窓口」等について本基本方針を策定し、必要に応じて見直しています。</p>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（2）個人データの安全管理に係る取扱規程の整備</h3>
+                </div>
+                <p>取得、利用、保存、提供、削除・廃棄等の段階ごとに、取扱方法、責任者・担当者およびその任務等についての規程を整備し、必要に応じて見直しています。</p>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（3）組織的安全管理措置</h3>
+                </div>
+                <ul>
+                    <li>個人データの管理責任者等の設置</li>
+                    <li>就業規則等における安全管理措置の整備</li>
+                    <li>個人データの安全管理に係る取扱規程に従った運用</li>
+                    <li>個人データの取扱状況を確認できる手段の整備</li>
+                    <li>個人データの取扱状況の点検及び監査体制の整備と実施</li>
+                    <li>漏えい等事案に対応する体制の整備</li>
+                </ul>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（4）人的安全管理措置</h3>
+                </div>
+                <ul>
+                    <li>従業者との個人データの非開示契約等の締結</li>
+                    <li>従業者の役割・責任等の明確化</li>
+                    <li>従業者への安全管理措置の周知徹底、教育及び訓練</li>
+                    <li>従業者による個人データ管理手続の遵守状況の確認</li>
+                </ul>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（5）物理的安全管理措置</h3>
+                </div>
+                <ul>
+                    <li>個人データの取扱区域等の管理</li>
+                    <li>従業機器及び電子媒体等の盗難等の防止者の役割・責任等の明確化</li>
+                    <li>電子媒体等を持ち運ぶ場合の漏えい等の防止</li>
+                    <li>個人データの削除及び機器、電子媒体等の廃棄</li>
+                </ul>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（6）技術的安全管理措置</h3>
+                </div>
+                <ul>
+                    <li>個人データの利用者の識別及び認証</li>
+                    <li>個人データの管理区分の設定及びアクセス制御</li>
+                    <li>個人データへのアクセス権限の管理</li>
+                    <li>個人データの漏えい・毀損等防止策</li>
+                    <li>個人データへのアクセスの記録及び分析</li>
+                    <li>個人データを取り扱う情報システムの稼動状況の記録及び分析</li>
+                    <li>個人データを取り扱う情報システムの監視及び監査</li>
+                </ul>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（7）委託先の監督</h3>
+                </div>
+                <p>個人データの取扱いを委託する場合には、個人データを適正に取り扱っている者を選定し、委託先における安全管理措置の実施を確保するため、外部委託に係る取扱規程を整備し、定期的に見直しています。</p>
+
+                <div class="p-privacyContents__numberList3-num-title">
+                    <h3>（8）外的環境の把握</h3>
+                </div>
+                <p>個人データを取り扱う国における個人情報の保護に関する制度を把握した上で安全管理措置を実施しています。</p>
+            </div>
+        </div>
+
+
+
+
+
+
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>事故多発交差点</b>
+              <p>事故が多発している交差点を「曜日別」「時間帯別」など１１項目で分析しているコーナーです。お近くを通る際の情報としてご参考ください。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.sonpo.or.jp/protection/kousaten/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>安全運転のポイント</b>
+              <p>安全運転や事故防止のポイントを時事テーマをもとにまとめています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.dairitenhp.com/anzen/anzen_top.htm" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>日本損害保険協会</b>
+              <p>自動車の事故や災害に遭ったときに役立つ情報、防災情報および各種統計等を提供しています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.sonpo.or.jp/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>生命保険協会</b>
+              <p>生命保険の基礎知識、生命保険協会からの最新のニュース等を提供しています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.seiho.or.jp/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>厚生労働省-先進医療</b>
+              <p>厚生労働省のホームページから先進医療についての情報を提供しています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.mhlw.go.jp/topics/bukyoku/isei/sensiniryo/index.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>生命保険文化センター</b>
+              <p>生命保険に関する全国実態調査についての情報提供しています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.jili.or.jp/research/report/zenkokujittai.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>ゴールドリボン・ネットワーク</b>
+              <p>ゴールドリボン・ネットワークは、小児がんの治療法や薬、経験者の生活の質（QOL）向上のための
+              研究開発を支援する非営利団体です。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.goldribbon.jp/index.htm" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>J.POSH 日本乳がんピンクリボン運動</b>
+              <p>私たちのスローガンである”for the future generation”明日を担う次の世代のために、
+              明日の健康な社会を残せるよう活動しています。</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.j-posh.com/index.html" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="p-privacy-policyContents__privacy-policyBox-whiteArea">
+            <div class="p-privacy-policyContents__privacy-policyBox-whiteArea-inner">
+              <b>日本脳卒中協会</b>
+              <p>脳卒中に関する正しい知識の普及および社会啓発によって新たな脳卒中発症の減少に寄与すると同時に、
+                脳卒中患者・家族に対する情報提供、自立のための支援を促進することによって、患者・家族の不安と
+                ハンディキャップの軽減と生活の質の改善を図り、その成果を通じて国民の保健、福祉の向上に寄与して行く事が目的…</p>
+              <div class="c-privacy-policyContents__privacy-policyBox--btn">
+                <a href="http://www.jsa-web.org/" rel="noopener noreferrer" target="_blank">
+                  <span>詳しくはこちらをチェック！</span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
