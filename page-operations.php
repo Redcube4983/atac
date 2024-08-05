01340931a0aab6f033909ebfd9120d51b832eb19
Redcube4983 <it@redcube.jp>
2024-08-05 18:54:55 +0900

    waka

--- page-operations.php
+++ page-operations.php
@@ -4,5 +4,41 @@ Template Name: operations
 */
 ?>
 <?php get_header(); ?>
+<main>
+  <section id="l-internalTop">
+    <h1 class="l-internalTop__title">
+      <div class="l-internalTop__text">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/pageTitle.webp" type="image/webp">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/pageTitle.png" alt="プライバシーポリシー">
+        </picture>
+      </div>
+      <div class="l-internalTop__underLine">
+        <picture>
+          <source srcset="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/ornamentLine.webp" type="image/webp">
+          <img src="<?php echo get_template_directory_uri(); ?>/images/privacy-policy/ornamentLine.png" alt="">
+        </picture>
+      </div>
+    </h1>
+  </section>
 
+  <section id="p-privacyContents">
+    <div class="p-privacyContents__privacyArea">
+      <div class="c-contentsLayout">
+        <div class="p-privacyContents__privacyBox">
+          <div class="p-privacyContents__privacyBox-title">
+              <h2>個人情報保護法宣言</h2>
+          </div>
+          <div class="p-privacyContents__privacyBox-inner">
+            <p>
+            当代理店は、個人情報保護の重要性に鑑み、また、保険業に対する社会の信頼をより向上させるため、個人情報の保護に関する法律（個人情報保護法）、行政手続における特定の個人を識別するための番号の利用等に関する法律（番号法）その他の関連法令、関係官庁からのガイドライン、特定個人情報の適正な取り扱いに関するガイドライン等を遵守して、個人情報を適正に取り扱うとともに、安全管理について適切な措置を講じます。 当代理店は、個人情報の取り扱いが適正に行われるよう、従業者への教育・指導を徹底し、適正な取り扱いに取り組んでまいります。
+            また、個人情報の取り扱いに関する苦情・相談に迅速に対応し、当代理店の個人情報の取り扱いおよび安全管理に係る適切な措置については、適宜見直し、改善いたします。
+            </p>
+          </div>
+        </div>
+        
+      </div>
+    </div>
+  </section>
+</main>
 <?php get_footer(); ?>
