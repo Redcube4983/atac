56ea13538366a8219f3752c0461720fc6a867ce5
Redcube4983 <it@redcube.jp>
2024-09-10 18:36:14 +0900

    waka

--- single-blog.php
+++ single-blog.php
@@ -82,11 +82,33 @@ Template Name: single-blog
                     <h3>Archives</h3><span>アーカイブ</span>
                 </div>
                 <ul class="post_archive">
-                <?php // 年別アーカイブのリストを出力
-                wp_get_archives( array(
-                'type' => 'yearly'
-                ) ); ?>
-                <?php wp_get_archives($args); ?>
+                <?php
+                    // 月別アーカイブリストを表示
+                    $post_type = 'blog'; // ポストタイプを指定
+                    $args = array(
+                        'post_type' => $post_type,
+                        'posts_per_page' => -1
+                    );
+                    $archive_query = new WP_Query( $args );
+                    while ( $archive_query->have_posts() ) {
+                        $archive_query->the_post();
+                        $archive_list[ get_the_time( 'Y/n', $post->ID ) ][] = $post->post_title;
+                    }
+                    wp_reset_postdata();
+                    if($archive_list):
+                ?>
+                <ul>
+                    <?php
+                    foreach( $archive_list as $year_month => $archive ) :
+                    $year_month_arr = explode( '/', $year_month );
+                    ?>
+                <li>
+                    <a href="<?php echo home_url( '/', 'http' ).$post_type.'/date/'.$year_month; ?>">
+                    <?php echo $year_month_arr[0].'年'.$year_month_arr[1].'月' ?> [<?php echo count( $archive ) ?>]</a>
+                </li>
+                    <?php endforeach; ?>
+                </ul>
+                <?php endif; ?>
                 </ul>
             </div>
             <div class="p-categoryList">
